c2d30ab4b03ace665c539ef7e541b204c7263468
larmberdo <690210986@qq.com>
2025-06-12 13:16:11 +0800

    配置GitHub Pages自动部署

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  // GitHub Pages部署配置
+  basePath: process.env.NODE_ENV === 'production' ? '/mantis-bug-tracker' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/mantis-bug-tracker/' : '',
 }
 
 module.exports = nextConfig 
